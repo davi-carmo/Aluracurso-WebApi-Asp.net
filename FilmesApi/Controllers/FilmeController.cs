91c56972c7da023402107cbd30380bac0ca3dc34
davip <davip@DESKTOP-LMNE0VM>
2023-08-23 22:56:49 -0400

    Add project files.

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,41 @@
+﻿using Filmes.Models;
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filmes.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class FilmeController : ControllerBase
+{
+
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 1;
+  
+    [HttpPost]
+    public IActionResult AdicionarFilme([FromBody] Filme filme) 
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+       return CreatedAtAction(nameof(RecuperaFilmePorId),
+            new {id = filme.Id, filme});
+       
+    }
+
+    [HttpGet]
+
+    public IEnumerable<Filme> RecuperarFilmes([FromQuery]int skip = 0,
+    [FromQuery]int take =50)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorId(int id) 
+    {
+      var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filmes is null) return NotFound();
+        return Ok(filme);
+    }
+}
